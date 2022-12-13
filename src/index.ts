1d6cffc8dbc22c745d424047d13638729883df7b
PawelObrzut <pawel.r.obrzut@gmail.com>
2022-12-13 22:53:37 +0100

    adds pagination

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const search = document.querySelector<HTMLElement>('.input-form__querry');
 const go = document.querySelector<HTMLElement>('.input-form__btn');
 const resultContainer = document.querySelector<HTMLElement>('.container');
 const searchList = document.querySelector<HTMLElement>('#recent-search-queries');
+const pages = document.querySelector<HTMLUListElement>('.pages');
 
 const api = createApi({
   accessKey: process.env.API_KEY
@@ -18,13 +19,19 @@ type Photo = {
 };
 
 interface State {
+  searchQuery: string;
   recentSearches: string[];
   pictures: Photo[];
+  currentPage: number;
+  total_pages: number | null;
 };
 
 let state: State = {
+  searchQuery: '',
   recentSearches: JSON.parse(localStorage.getItem('recentSearches')) || [],
   pictures: [],
+  currentPage: 0,
+  total_pages: null,
 };
 
 const update = (newState: State) => {
@@ -43,7 +50,7 @@ const addSearchHistory = (recentSearches: string[]) => {
     optionTag.setAttribute('value', query);
     optionTag.innerText = query;
     searchList.appendChild(optionTag);
-  })
+  });
 }
 
 const renderImage = (child: string, parent: HTMLElement) => {
@@ -71,11 +78,24 @@ const displayPics = (pictures: Photo[]) => {
   });
 };
 
+const paginationTemplate = () =>{
+  return `
+    <li class="prevPage"> < PREV </li>
+    <li class="currentPage">0</li>
+    <li class="nextPage"> NEXT > </li>
+  `
+};
+
+const displayPagination = () => {
+  pages.innerHTML = '';
+  pages.innerHTML += paginationTemplate();
+};
+
 const conductGallerySearch = () => {
   api.search
     .getPhotos({
-      query: (search as HTMLInputElement).value,
-      page: 1,
+      query: state.searchQuery,
+      page: state.currentPage,
       perPage: 10,
       orientation: 'landscape',
     })
@@ -89,8 +109,11 @@ const conductGallerySearch = () => {
       });
 
       update({
+        searchQuery: state.searchQuery,
         recentSearches: JSON.parse(localStorage.getItem('recentSearches')) || [],
         pictures: resultPics,
+        currentPage: state.currentPage,
+        total_pages: result.response.total_pages,
       });
 
       (search as HTMLInputElement).value = '';
@@ -107,13 +130,30 @@ go.addEventListener('click', event => {
     state.recentSearches.pop();
   }
   saveRecentSearches(state.recentSearches);
+  state.searchQuery = (search as HTMLInputElement).value;
   conductGallerySearch();
+  state.currentPage = state.currentPage + 1;
 });
+
+pages.addEventListener('click', (event: Event) => {
+  if ((event.target as Element).matches('.prevPage')) {
+    console.log('prev page');
+    state.currentPage = state.currentPage - 1;
+    conductGallerySearch();
+  }
+  if ((event.target as Element).matches('.nextPage')) {
+    console.log('next page');
+    state.currentPage = state.currentPage + 1;
+    console.log(state);
+    conductGallerySearch();
+  }
+})
 
 window.addEventListener('statechange', () => {
   resultContainer.innerHTML += '';
   displayPics(state.pictures);
   addSearchHistory(state.recentSearches);
+  displayPagination();
 });
 
 window.dispatchEvent(new Event('statechange'));
